adea69ff2b6e018955d541afe9b2a80d80279435
shubham kumar <shubhammank@gmail.com>
2022-07-26 21:20:41 +0530

    ll

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,7 +5,10 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature className="titleNM" title="About us" text="Connecting the dots is an established services provider and we opened our doors since 2017.Our team has many more years of industry experience and is qualified to recommend the right solution for your technical problems. We specialize in working with our clients to provide the right mix of technical services in alliance with their own in-house technical team. We can do as little or as much as you need. " />
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">About us</h1>
+      </div>
+      <Feature text="Connecting the dots is an established services provider and we opened our doors since 2017.Our team has many more years of industry experience and is qualified to recommend the right solution for your technical problems. We specialize in working with our clients to provide the right mix of technical services in alliance with their own in-house technical team. We can do as little or as much as you need. " />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">Household services</h1>
